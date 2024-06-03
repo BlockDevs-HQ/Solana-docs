d9279ad27b8772fe6cf1929042b920907e172332
Samarth <sam@Samarths-MacBook-Air.local>
2024-06-03 14:38:22 +0530

    rewapping of files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
-});
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
 
-module.exports = withNextra({
-  pageExtensions: ["mdx", "md", "jsx", "js", "ts", "tsx"],
-});
+module.exports = withNextra()
