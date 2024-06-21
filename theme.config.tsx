cd0d56e9c0d81500780a1d92a1bab8013748bcda
Samarth <sam@Samarths-MacBook-Air.local>
2024-06-21 12:40:54 +0530

    Fixed link

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { FaTelegramPlane } from "react-icons/fa";
 
 const config: DocsThemeConfig = {
   logo: <span>BlockDevs</span>,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/SamarthSaxena10/Solana-X-BlockDevs",
   },
   chat: {
-    link: "https://discord.com",
+    link: "https://t.me/+IeQ3sKQ_LIU4MzY9",
+    icon: <FaTelegramPlane />,
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
 };
 
 export default config;
